7dee7c48703a4d6ce75b788235d308033074b891
nathalie_raffray <nathalie@3dverse.com>
2023-02-07 00:30:12 -0500

    Prompt user for audio input device and output device

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@
 
 
 //--------------------------------------------------------------------------------------------------
+enum class device_type
+{
+    input,
+    output
+};
+
+//--------------------------------------------------------------------------------------------------
 std::vector<char> read_file(const std::string &filePath)
 {
     std::ifstream file(filePath, std::ios::binary | std::ios::ate);
@@ -87,6 +94,73 @@ bool try_end_playing_audio(RtAudio &dac)
 }
 
 //--------------------------------------------------------------------------------------------------
+bool is_correct_device_type(const RtAudio::DeviceInfo &info, device_type deviceType)
+{
+    return deviceType == device_type::input ? info.inputChannels > 0 : info.outputChannels > 0;
+}
+
+//--------------------------------------------------------------------------------------------------
+bool is_valid_device_id(int id, device_type deviceType, RtAudio &dac)
+{
+    const auto deviceCount = static_cast<int32_t>(dac.getDeviceCount());
+    if (id < 0 || id >= deviceCount)
+    {
+        return false;
+    }
+
+    return is_correct_device_type(dac.getDeviceInfo(id), deviceType);
+}
+
+//--------------------------------------------------------------------------------------------------
+void list_devices_of_type(device_type deviceType, RtAudio &dac)
+{
+    RtAudio::DeviceInfo info;
+    for (auto i = 0u; i < dac.getDeviceCount(); i++)
+    {
+        info = dac.getDeviceInfo(i);
+        if (info.probed == true && is_correct_device_type(info, deviceType))
+        {
+            std::cout << i << ": " << info.name << std::endl;
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+int get_valid_device_id_from_user(device_type deviceType, RtAudio &dac)
+{
+    auto userInput = std::string("");
+    std::cin >> userInput;
+
+    while (!is_valid_device_id(std::stoi(userInput), deviceType, dac))
+    {
+        std::cout << "Invalid input. Try again." << std::endl;
+        std::cin >> userInput;
+    }
+
+    // clear cin
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    return std::stoi(userInput);
+}
+
+//--------------------------------------------------------------------------------------------------
+int get_user_specified_device_id(RtAudio &dac, device_type deviceType)
+{
+    const auto deviceTypeStr = deviceType == device_type::input ? "input" : "output";
+    std::cout << "Please specify from the list below your chosen " << deviceTypeStr << " device. "
+        "Enter the number preceding it to choose it." << std::endl;
+
+    list_devices_of_type(deviceType, dac);
+
+    const auto deviceId = get_valid_device_id_from_user(deviceType, dac);
+
+    std::cout << "Your chosen " << deviceTypeStr << " device is " 
+        << dac.getDeviceInfo(deviceId).name << std::endl << std::endl;
+
+    return deviceId;
+}
+
+//--------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
     const auto filePath = "../../res/input/i_am_sitting_in_a_room.mp3";
@@ -116,6 +190,9 @@ int main(int argc, char *argv[])
         std::cout << "Error: No audio devices found!" << std::endl;
         return -1;
     }
+
+    const auto chosenInputDeviceId  = get_user_specified_device_id(dac, device_type::input);
+    const auto chosenOutputDeviceId = get_user_specified_device_id(dac, device_type::output);
 
     if (!try_begin_playing_audio(dac, decoder))
     {
