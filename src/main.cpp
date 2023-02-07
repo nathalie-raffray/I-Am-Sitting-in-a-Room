e4034d976262a43c635498065542e79424ba4190
nathalie_raffray <nathalie@3dverse.com>
2023-02-06 22:52:56 -0500

    Reads mp3 file (i_am_sitting_in_a_room.mp3) and plays it back

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,137 @@
-#include <iostream>
+#include <fstream>
+#include <filesystem>
 
-#include "RtAudio.h"
+#include "rtaudio/RtAudio.h"
 
+#include "mp3_decoder.hpp"
+
+
+//--------------------------------------------------------------------------------------------------
+std::vector<char> read_file(const std::string &filePath)
+{
+    std::ifstream file(filePath, std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buffer(size);
+    if (!file.read(buffer.data(), size))
+    {
+        return {};
+    }
+
+    return buffer;
+}
+
+//--------------------------------------------------------------------------------------------------
+int sing_song(void *pOutputBuffer, void *pInputBuffer, unsigned int nBufferFrames,
+    double streamTime, RtAudioStreamStatus status, void *pUserData)
+{
+    if (status)
+    {
+        std::cout << "Stream underflow detected!" << std::endl;
+    }
+
+    auto pOutputFloatBuffer = reinterpret_cast<float *>(pOutputBuffer);
+    auto pMp3Decoder        = reinterpret_cast<mp3_decoder *>(pUserData);
+
+    pMp3Decoder->readPcmFrames(nBufferFrames, pOutputFloatBuffer);
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------------------------------
+bool try_begin_playing_audio(RtAudio &dac, mp3_decoder &decoder)
+{
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId     = dac.getDefaultOutputDevice();
+    parameters.nChannels    = decoder.getChannelCount();
+    parameters.firstChannel = 0;
+    const auto sampleRate   = decoder.getSampleFrequency();
+    auto bufferFrames       = 256u; // 256 sample frames
+
+    try 
+    {
+        dac.openStream(&parameters, NULL, RTAUDIO_FLOAT32,
+            sampleRate, &bufferFrames, &sing_song, (void *)&decoder);
+        dac.startStream();
+    }
+    catch (RtAudioError &e)
+    {
+        e.printMessage();
+        return false;
+    }
+
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------------
+bool try_end_playing_audio(RtAudio &dac)
+{
+    auto success = true;
+    try
+    {
+        dac.stopStream();
+    }
+    catch (RtAudioError &e)
+    {
+        e.printMessage();
+        success = false;
+    }
+
+    if (dac.isStreamOpen())
+    {
+        dac.closeStream();
+    }
+
+    return success;
+}
 
 //--------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-    RtAudio audio;
-    // Determine the number of devices available
-    unsigned int devices = audio.getDeviceCount();
-    // Scan through devices for various capabilities
-    RtAudio::DeviceInfo info;
-    for (unsigned int i = 0; i < devices; i++)
+    const auto filePath = "../../res/input/i_am_sitting_in_a_room.mp3";
+    if (!std::filesystem::exists(filePath))
     {
-        info = audio.getDeviceInfo(i);
-        if (info.probed == true)
-        {
-            // Print, for example, the maximum number of output channels for each device
-            std::cout << "device = " << i;
-            std::cout << ": maximum output channels = " << info.outputChannels << "\n";
-        }
+        std::cout << "Error: The file path provided (" << filePath << ") doesn't exist." << std::endl;
+        return -1;
     }
+
+    auto mp3Bytes = read_file(filePath);
+    if (mp3Bytes.empty())
+    {
+        std::cout << "Error: Failed to read the contents of file at " << filePath << "." << std::endl;
+        return -1;
+    }
+
+    mp3_decoder decoder(mp3Bytes.data(), mp3Bytes.size());
+    if (!decoder.isValid())
+    {
+        std::cout << "Error: Failed to decode mp3 file " << filePath << "." << std::endl;
+        return -1;
+    }
+
+    RtAudio dac;
+    if (dac.getDeviceCount() < 1) 
+    {
+        std::cout << "Error: No audio devices found!" << std::endl;
+        return -1;
+    }
+
+    if (!try_begin_playing_audio(dac, decoder))
+    {
+        std::cout << "Error: Failed to play audio." << std::endl;
+        return -1;
+    }
+    
+    char input;
+    std::cout << "Playing ... press <enter> to quit." << std::endl;
+    std::cin.get(input);
+
+    if (!try_end_playing_audio(dac))
+    {
+        std::cout << "Error: Failed to stop stream." << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
---
+++ src/mp3_decoder.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <cstdint>
+
+#define DR_MP3_IMPLEMENTATION
+#include "dr_libs/dr_mp3.h"
+
+
+//----------------------------------------------------------------------------------------------
+class mp3_decoder
+{
+public:
+    //------------------------------------------------------------------------------------------
+    mp3_decoder(const void *pData, size_t dataSize)
+    {
+        isValid_ = drmp3_init_memory(&decoder_, pData, dataSize, NULL);
+        sampleCount_ = isValid_ ? drmp3_get_pcm_frame_count(&decoder_) : 0;
+    }
+
+    //------------------------------------------------------------------------------------------
+    ~mp3_decoder()
+    {
+        drmp3_uninit(&decoder_);
+    }
+
+    //------------------------------------------------------------------------------------------
+    bool isValid() const
+    {
+        return isValid_;
+    }
+
+    //------------------------------------------------------------------------------------------
+    uint64_t getSampleCount() const
+    {
+        return sampleCount_;
+    }
+
+    //------------------------------------------------------------------------------------------
+    uint8_t getChannelCount() const
+    {
+        return uint8_t(decoder_.channels);
+    }
+
+    //------------------------------------------------------------------------------------------
+    uint32_t getSampleFrequency() const
+    {
+        return decoder_.sampleRate;
+    }
+
+    //------------------------------------------------------------------------------------------
+    void readPcmFrames(uint64_t framesToRead, float *pBufferOut)
+    {
+        drmp3_read_pcm_frames_f32(&decoder_, framesToRead, pBufferOut);
+    }
+
+private:
+    //------------------------------------------------------------------------------------------
+    drmp3       decoder_;
+    //------------------------------------------------------------------------------------------
+    bool        isValid_;
+    //------------------------------------------------------------------------------------------
+    uint64_t    sampleCount_;
+};
